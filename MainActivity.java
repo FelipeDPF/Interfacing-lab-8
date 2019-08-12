80d7c94ee4d5e48115f75e3f8b35312f8a49c8b5
Felipe Filho <44655339+FelipeDPF@users.noreply.github.com>
2019-08-12 18:54:31 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,88 @@
+package com.algonquincollege.depa0028.loginexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.Button;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private EditText editUser;
+    private EditText editPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.Login);
+        editUser = findViewById(R.id.User);
+        editPassword = findViewById(R.id.editText);
+
+        // register an anonymous inner class as the event handler for the loginButton
+        loginButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (!validateUsername(editUser.getText().toString())) {
+                    editUser.requestFocus();
+                } else if (!validatePassword(editPassword.getText().toString())) {
+                    editPassword.requestFocus();
+                } else {
+                    // handle the button click: display a Toast message to the user.
+                    String display = "User Name: " + editUser.getText() + "\n" + "Password: " + editPassword.getText();
+                    Toast.makeText(getApplicationContext(), display, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    protected boolean validateUsername(String username) {
+        int i = 4;
+        if (username.isEmpty()) {
+            editUser.setError("The username is mandatory");
+            return false;
+        } else if (username.length() != 8) {
+            editUser.setError("username must be exactly 8 characters");
+            return false;
+        } else if ((!Character.isAlphabetic(username.charAt(0)) || Character.isUpperCase(username.charAt(0))) ||
+                (!Character.isAlphabetic(username.charAt(1)) || Character.isUpperCase(username.charAt(1)))) {
+            editUser.setError("can only be lower-case alphabetic characters (a-z)");
+            return false;
+        } else if ((!Character.isAlphabetic(username.charAt(2)) && !Character.isDigit(username.charAt(2))) ||
+                (!Character.isAlphabetic(username.charAt(3)) && !Character.isDigit(username.charAt(3)))) {
+            editUser.setError("can only be lower-case alphabetic characters (a-z) or numbers");
+            return false;
+        } else {
+            while (i < username.length()) {
+                if (username.charAt(i) < '0' || username.charAt(i) > '9') {
+                    editUser.setError("positions 5 to 8 must be digits (0-9)");
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+    
+    protected boolean validatePassword(String password) {
+        if (password.isEmpty()) {
+            editPassword.setError("The password is mandatory");
+            return false;
+        }else if(password.length() < 5){
+            editPassword.setError("To short (minimum of 5 numbers)");
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
